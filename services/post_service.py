36e7cc6cde2420471376b4638c596993b4b73a17
CHRIST MAYOMBO <trianalyser@gmail.com>
2026-03-14 22:54:30 +0100

    Update post_service.py

--- services/post_service.py
+++ services/post_service.py
@@ -1,20 +1,32 @@
+import uuid
 from core.supabase_client import supabase
+from datetime import datetime
 
 
-def get_feed(limit=20):
+def upload_video(user_id, file):
 
-    res = supabase.table("posts").select(
+    ext = file.name.split(".")[-1]
 
-        """
-        id,
-        text,
-        media_path,
-        created_at,
-        like_count,
-        comment_count,
-        profiles(username, profile_pic)
-        """
+    filename = f"posts/{user_id}/{uuid.uuid4()}.{ext}"
 
-    ).order("created_at", desc=True).limit(limit).execute()
+    supabase.storage.from_("media").upload(
+        filename,
+        file.getvalue(),
+        {"content-type": f"video/{ext}"}
+    )
 
-    return res.data
+    return filename
+
+
+def create_post(user_id, text, media_path=None):
+
+    supabase.table("posts").insert({
+
+        "user_id": user_id,
+        "text": text,
+        "media_path": media_path,
+        "created_at": datetime.utcnow().isoformat(),
+        "like_count": 0,
+        "comment_count": 0
+
+    }).execute()
